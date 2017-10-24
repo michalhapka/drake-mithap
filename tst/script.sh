3e1d9639b4c68aaabef3a1c92869584d2007ef71
Michal Przybytek <mitek@tiger.chem.uw.edu.pl>
2017-10-24 17:17:44 +0200

    Job testowy

---
+++ tst/script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+threelQ=/home/mitek/AA/geminal_slater/drake-mithap/other/Quch/threel
+
+case $1 in
+"L")
+    $threelL $2 > /dev/null
+    case $3 in
+    "J")
+	mv fileJ0.F $4
+	;;
+    "K")
+	mv fileK0.F $4
+	mv fileK1.F $5
+	;;
+    *) 
+	echo "Unknown integral class!!!"
+	;;
+    esac
+    ;;
+"P")
+    $threelP $2 
+    ;;
+"Q")
+    $threelQ $2 
+    ;;
+*)
+    echo "Unknown three-electron program!!!"
+    ;;
+esac
+
